2804eb6899c7eda2959c0a3b9df4e55012042fd7
Renato <renatorp2005@gmail.com>
2026-06-21 19:43:46 -0300

    atualização do scipt de estatisticas h2

--- scripts/analysis/estatisticas_h1.py
+++ scripts/analysis/estatisticas_h1.py
@@ -30,3 +30,87 @@ plt.savefig(
     dpi=300,
     bbox_inches="tight"
 )
+##histograma de renda##
+plt.figure(figsize=(8,5))
+plt.hist(df["Income"], bins=30)
+
+plt.xlabel("Renda")
+plt.ylabel("Frequência")
+plt.title("Distribuição da renda")
+
+plt.savefig(
+    "outputs/graphs/histograma_renda.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+##boxplot para visualizar a distribuição da renda##
+plt.figure(figsize=(8,5))
+plt.boxplot(df["Income"])
+
+plt.title("Boxplot da renda")
+
+plt.savefig(
+    "outputs/graphs/boxplot_renda.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+##histograma de anos de estudo##
+plt.figure(figsize=(10, 6))
+
+plt.hist(df["Years_of_study"], bins=16)
+
+plt.title("Distribuição dos Anos de Estudo")
+plt.xlabel("Anos de Estudo")
+plt.ylabel("Frequência")
+
+plt.savefig(
+    "outputs/graphs/histograma_anos_estudo.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+
+plt.close()
+##boxplot para visualizar a distribuição dos anos de estudo##
+plt.figure(figsize=(8, 6))
+
+plt.boxplot(df["Years_of_study"])
+
+plt.title("Boxplot dos Anos de Estudo")
+plt.ylabel("Anos de Estudo")
+
+plt.savefig(
+    "outputs/graphs/boxplot_anos_estudo.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+
+plt.close()
+##grafico de dispersão (anos de estudo vs renda)##
+plt.figure(figsize=(10, 6))
+
+plt.scatter(
+    df["Years_of_study"],
+    df["Income"],
+    alpha=0.3
+)
+
+plt.title("Relação entre Anos de Estudo e Renda")
+plt.xlabel("Anos de Estudo")
+plt.ylabel("Renda")
+
+plt.savefig(
+    "outputs/graphs/dispersao_estudo_renda.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+
+plt.close()
+##Parametro##
+alpha=0.3
+##teste de pearson para verificar a correlação entre anos de estudo e renda##
+from scipy.stats import pearsonr
+
+r, p = pearsonr(df["Years_of_study"], df["Income"])
+
+print(f"Correlação (r): {r:.4f}")
+print(f"P-valor: {p:.6f}")
---
+++ scripts/analysis/estatisticas_h2.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+
+# Carregar dados
+df = pd.read_csv("data/processed/pnad_processed.correl.estud.e.renda.csv")
+
+# Criar grupos educacionais
+condicoes = [
+    df["Years_of_study"] <= 8,
+    (df["Years_of_study"] >= 9) & (df["Years_of_study"] <= 11),
+    df["Years_of_study"] == 12,
+    df["Years_of_study"] >= 13
+]
+
+grupos = [
+    "Fundamental incompleto",
+    "Fundamental completo",
+    "Ensino médio",
+    "Ensino superior"
+]
+
+df["Education_Level"] = np.select(condicoes, grupos, default="Não classificado")
+
+# Conferir quantidade por grupo
+print(df["Education_Level"].value_counts())
+
+# Analise de variância (ANOVA)##
+from scipy.stats import f_oneway
+
+fund_inc = df[df["Education_Level"]=="Fundamental incompleto"]["Income"]
+fund_comp = df[df["Education_Level"]=="Fundamental completo"]["Income"]
+medio = df[df["Education_Level"]=="Ensino médio"]["Income"]
+superior = df[df["Education_Level"]=="Ensino superior"]["Income"]
+
+f_stat, p_valor = f_oneway(
+    fund_inc,
+    fund_comp,
+    medio,
+    superior
+)
+
+print(f"Estatística F: {f_stat:.4f}")
+print(f"P-valor: {p_valor:.10f}")
